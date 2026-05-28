703c8004963e2baa2955ec1746b7c67b9e0b0455
dcostap <dariocostapecalama@gmail.com>
2026-05-28 14:54:31 +0200

    Copy packaged GUI zip to shared deployment folder

--- package-gui-for-users.bat
+++ package-gui-for-users.bat
@@ -13,8 +13,23 @@ call npm run build:release --workspace apps/gui
 if errorlevel 1 goto :error
 
 echo.
+echo Copying final portable zip to \\stellae-21\sw$ ...
+set "ARTIFACT_DIR=apps\gui\artifacts\electron"
+set "PORTABLE_ZIP="
+for /f "delims=" %%F in ('dir /b /a-d /o-d "%ARTIFACT_DIR%\*.zip" 2^>nul') do (
+  if not defined PORTABLE_ZIP set "PORTABLE_ZIP=%%F"
+)
+if not defined PORTABLE_ZIP (
+  echo No portable zip found in %ARTIFACT_DIR%.
+  goto :error
+)
+copy /y "%ARTIFACT_DIR%\%PORTABLE_ZIP%" "\\stellae-21\sw$\%PORTABLE_ZIP%"
+if errorlevel 1 goto :error
+
+echo.
 echo Done. Release artifacts are in:
-echo apps\gui\artifacts\electron
+echo %ARTIFACT_DIR%
+echo Copied portable zip to \\stellae-21\sw$\%PORTABLE_ZIP%
 exit /b 0
 
 :error
